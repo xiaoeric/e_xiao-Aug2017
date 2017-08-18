9ffc90a1dd407808c7a3ebe16f20f7803f395581
xiaoeric <alpha2275@gmail.com>
2017-08-18 09:39:40 -0700

    8/18/2017 Hello world demonstration
    I found the program to be simple I suppose

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,13 @@
+/**
+ * Write a description of class Hello_world here.
+ * 
+ * @author Eric Xiao
+ * @version 08.18.2017
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Eric Xiao"); //Display the string
+    }
+}
