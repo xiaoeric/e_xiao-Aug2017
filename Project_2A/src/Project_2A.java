72d73f82b176968cc067a5826bc0ec8c8072ae54
xiaoeric <alpha2275@gmail.com>
2017-08-24 09:33:59 -0700

    Commit message

---
+++ Project_2A/src/Project_2A.java
@@ -0,0 +1,12 @@
+//Eric Xiao
+//8/23/2017
+//Project 2 -- Use print and println
+public class Project_2A {
+
+	public static void main(String[] args) {
+		System.out.print("Hi there");
+		System.out.println("Hi there");
+		System.out.println("Hi there again");
+	}
+
+}
