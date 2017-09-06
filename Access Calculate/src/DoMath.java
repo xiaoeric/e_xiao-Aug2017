b8429eba2f33ba2eefa113ece2920abd3c211272
xiaoeric <alpha2275@gmail.com>
2017-09-06 10:46:23 -0700

    Completed Part 1

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -1,6 +1,18 @@
-
+/*Eric Xiao
+ * September 6, 2017
+ * This is the runner for the calculate library. It is used to test the methods in Calculate.
+ */
 public class DoMath {
 	public static void main(String[] args) {
 		System.out.println(Calculate.square(5));
+		System.out.println(Calculate.cube(5));
+		System.out.println(Calculate.average(5, 8));
+		System.out.println(Calculate.average(5, 8, 11));
+		System.out.println(Calculate.toDegrees(3.14159));
+		System.out.println(Calculate.toRadians(180));
+		System.out.println(Calculate.discriminant(1, -2, 4));
+		System.out.println(Calculate.toImproperFrac(3, 1, 2));
+		System.out.println(Calculate.toMixedNum(7, 2));
+		System.out.println(Calculate.foil(2, 3, 6, -7, "n"));
 	}
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -1,6 +1,37 @@
-
+/*Eric Xiao
+ * September 6, 2017
+ * Our self written math library
+ * It contains a series of methods to do basic math functions.
+ */
 public class Calculate {
 	public static int square(int input) {
 		return input*input;
+	}
+	public static int cube(int input) {
+		return input*input*input;
+	}
+	public static double average(double input1, double input2) {
+		return (input1 + input2)/2;
+	}
+	public static double average(double input1, double input2, double input3) {
+		return (input1 + input2 + input3)/3;
+	}
+	public static double toDegrees(double input) {
+		return input / 3.141589 * 180;
+	}
+	public static double toRadians(double input) {
+		return input / 180 * 3.14159;
+	}
+	public static double discriminant(double a, double b, double c) {
+		return b * b - 4 * a * c;
+	}
+	public static String toImproperFrac(int wholeNum, int numerator, int denominator) {
+		return (wholeNum * denominator + numerator) + "/" + denominator;
+	}
+	public static String toMixedNum(int numerator, int denominator) {
+		return (numerator / denominator) + "_" + numerator % denominator + "/" + denominator;
+	}
+	public static String foil(int coeff1, int num1, int coeff2, int num2, String variable) {
+		return (coeff1 * coeff2) + variable + "^2" + " + " + (num1 * coeff2 + num2 * coeff1) + variable + " + " + (num1 * num2);
 	}
 }
