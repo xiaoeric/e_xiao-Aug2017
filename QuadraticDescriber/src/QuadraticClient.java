64261de0a553bf3890a66b56139cf0678c83226d
xiaoeric <alpha2275@gmail.com>
2017-09-27 11:19:38 -0700

    Implemented looping

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -14,20 +14,27 @@ import java.util.Scanner;
  * Accept user input for c, store as variable
  * Add a line for A E S T H E T I C S
  * Send a, b, and c to quadrDescriber and print the returned String
+ * Prompts the user if they want to continue or not
+ * Exits program if "quit", otherwise continue
  */
 public class QuadraticClient {
 
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
-		System.out.println("Given the quadratic equation: ax^2 + bx + c,");
-		System.out.print("Please enter a value for a: ");
-		int a = userInput.nextInt();
-		System.out.print("Please enter a value for b: ");
-		int b = userInput.nextInt();
-		System.out.print("Please enter a value for c: ");
-		int c = userInput.nextInt();
-		System.out.println();
-		System.out.println(Quadratic.quadrDescriber(a, b, c));
+		String willContinue = "";
+		do {
+			System.out.println("Given the quadratic equation: ax^2 + bx + c,");
+			System.out.print("Please enter a value for a: ");
+			int a = userInput.nextInt();
+			System.out.print("Please enter a value for b: ");
+			int b = userInput.nextInt();
+			System.out.print("Please enter a value for c: ");
+			int c = userInput.nextInt();
+			System.out.println();
+			System.out.println(Quadratic.quadrDescriber(a, b, c));
+			System.out.println();
+			System.out.println("Do you want to continue? Type \"quit\" to exit.");
+			willContinue = userInput.next();
+		} while(!willContinue.equals("quit"));
 	}
-
 }
