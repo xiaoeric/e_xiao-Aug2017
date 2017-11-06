fba5d72c1c25dec1b86afade7d7195001d0683ab
xiaoeric <alpha2275@gmail.com>
2017-11-06 09:51:35 -0800

    Did some stuff with Magpie

--- MagPie2/src/Magpie2.java
+++ MagPie2/src/Magpie2.java
@@ -13,13 +13,29 @@ public class Magpie2 {
 	 */
 	public String getResponse(String statement) {
 		String response = "";
-		if (statement.indexOf("no") >= 0) {
+		if (statement.trim().length() == 0) {
+			response = "... Are you gonna say something?";
+		} else if (statement.indexOf("no") >= 0) {
 			response = "Why so negative?";
 		} else if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
 			response = "Tell me more about your family.";
+		} else if (statement.indexOf("Louie") >= 0
+				|| statement.indexOf("Dreyer") >= 0
+				|| statement.indexOf("Cotter") >= 0
+				|| statement.indexOf("Battaglini") >= 0) {
+			response = "Wow she sounds like a great teacher!";
+		} else if (statement.indexOf("Jackson") >= 0
+				|| statement.indexOf("Phillips") >= 0) {
+			response = "Wow he sounds like a great teacher!";
+		} else if (statement.indexOf("happy") >= 0) {
+			response = ":)";
+		} else if (statement.indexOf("sad") >= 0) {
+			response = ":(";
+		} else if (statement.indexOf("angry") >= 0) {
+			response = ">:O";
 		} else {
 			response = getRandomResponse();
 		}
@@ -31,7 +47,7 @@ public class Magpie2 {
 	 * returns a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -44,6 +60,10 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) {
+			response = "Ok.";
+		} else if (whichResponse == 5) {
+			response = "Alright.";
 		}
 
 		return response;
