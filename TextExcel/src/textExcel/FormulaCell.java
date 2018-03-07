ccd167062aae8e814600c7e9055a396694fe7f4e
xiaoeric <alpha2275@gmail.com>
2018-03-07 11:12:41 -0800

    Checkpoint 4 complete!

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -8,14 +8,52 @@ public class FormulaCell extends RealCell {
 	
 	@Override
 	public String abbreviatedCellText() {
-		// TODO Auto-generated method stub
-		return "PLACEHOLD";
+		String text = Double.toString(getDoubleValue());
+		while(text.length() < 10) {
+			text += " ";
+		}
+		return text.substring(0, 10);
 	}
 
 	@Override
 	public double getDoubleValue() {
-		// TODO Auto-generated method stub
-		return 0;
+		String formula = userInput.substring(2, userInput.length() - 2);
+		return Double.parseDouble(evaluate(formula));
+	}
+	
+	public String evaluate(String formula) {
+		String[] arguments = formula.split(" ");
+		
+		if(arguments.length < 3) {
+			return arguments[0];
+		}
+		
+		double firstOperand = Double.parseDouble(arguments[0]);
+		double secondOperand = Double.parseDouble(arguments[2]);
+		String operator = arguments[1];
+		
+		String result;
+		
+		if(operator.equals("+")) {
+			result = Double.toString(firstOperand + secondOperand);
+		} else if(operator.equals("-")) {
+			result = Double.toString(firstOperand - secondOperand);
+		} else if(operator.equals("*")) {
+			result = Double.toString(firstOperand * secondOperand);
+		} else if(operator.equals("/")) {
+			result = Double.toString(firstOperand / secondOperand);
+		} else {
+			result = "ERROR";
+		}
+		
+		if(arguments.length > 3) {
+			for(int i = 3; i < arguments.length; i++) {
+				result += " " + arguments[i];
+			}
+			return evaluate(result);
+		} else {
+			return result;
+		}
 	}
 
 }
